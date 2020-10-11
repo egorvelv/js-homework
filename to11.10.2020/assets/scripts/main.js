61aeac23dd2060a4d39f212035b3253a3d749647
Falcon <grekwot976@gmail.com>
2020-10-11 18:57:45 +0300

    task to 11.10.2020

--- to11.10.2020/assets/scripts/main.js
+++ to11.10.2020/assets/scripts/main.js
@@ -6,21 +6,20 @@ let salaries = {
 
 let sum = 0;
 for (let key in salaries) {
-  sum + salaries[key];
-}
+  sum += salaries[key];
+};
 
 function isEmpty(obj) {
   for (let key in obj) {
     return false;
   }
   return true;
-
-}
+};
 
 function multiplyNumeric(obj) {
   for (let key in obj) {
     if (typeof obj[key] == 'number') {
-      obj[key] * 2;
+      obj[key] *= 2;
     }
   }
-}
+};
